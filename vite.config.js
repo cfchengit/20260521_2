04166bec93083f875a7a701cd0b39c7bcebe3f07
cfchen_tku <cfchen@mail.tku.edu.tw>
2026-05-21 22:05:25 +0800

    1.01

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/aqi-tracker/'   // ← 改成你的 GitHub Repository 名稱
+  base: '/20260521_2/'   // ← 改成你的 GitHub Repository 名稱
 })
